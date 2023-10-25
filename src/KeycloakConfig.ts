8e122d1f17197d94ecd01a274454122886c4c773
Louis Mullie <louis.mullie@gmail.com>
2023-10-25 04:24:51 -0400

    Configure externally

--- src/KeycloakConfig.ts
+++ src/KeycloakConfig.ts
@@ -2,7 +2,7 @@ const config = {
     "realm": process.env.CODA_HUB_API_AUTH_REALM,
     "auth-server-url": process.env.CODA_AUTH_SERVICE_URL,
     "ssl-required": "external",
-    "resource": "coda-hub-api",
+    "resource": process.env.CODA_HUB_API_AUTH_CLIENT_ID,
     "credentials": {
         "secret": process.env.CODA_HUB_API_AUTH_CLIENT_SECRET
     },
